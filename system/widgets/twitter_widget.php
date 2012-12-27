d29061c77653228054f74ec9b18e0e24ccb84d5e
Hilaladdiyar Muhammad Nur <hilaladdiyar@gmail.com>
2012-12-27 16:12:42 +0700

    fix bug in twitter widget

--- system/widgets/twitter_widget.php
+++ system/widgets/twitter_widget.php
@@ -39,6 +39,8 @@ class CFX_Twitter_Widget extends WP_Widget {
             'tweets_bg' => '000000',
             'tweets_color' => 'ffffff',
             'tweets_links' => '4aed05',
+			'tweets_num' => 4,
+			'show_avatar' => 'false',
         );
 
         $widget_ops = array(
@@ -47,9 +49,7 @@ class CFX_Twitter_Widget extends WP_Widget {
         );
 
         $control_ops = array(
-            'id_base' => 'twitter-widget',
-            'width' => 200,
-            'height' => 250,
+            'id_base' => 'twitter-widget'
         );
 
         $this->WP_Widget('twitter-widget', __('CalibreFx - Twitter Widget Box', 'calibrefx'), $widget_ops, $control_ops);
@@ -64,6 +64,8 @@ class CFX_Twitter_Widget extends WP_Widget {
     function widget($args, $instance) {
 		global $twitteruser;
 		
+		$twitteruser = calibrefx_get_option('twitter_username');
+		
         extract($args);
         $instance = wp_parse_args((array) $instance, $this->defaults);
 		
@@ -73,35 +75,43 @@ class CFX_Twitter_Widget extends WP_Widget {
 
         if (!empty($instance['title']))
             echo $before_title . apply_filters('widget_title', $instance['title'], $instance, $this->id_base) . $after_title;
+			
+		echo '<div id="twitter-widget-content"></div>';
 
         //Widget Body Start
         
         printf('<script>
-			new TWTR.Widget({
-			  version: 2,
-			  type: \'profile\',
-			  rpp: 4,
-			  interval: %1$s,
-			  width: %2$s,
-			  height: %3$s,
-			  theme: {
-				shell: {
-				  background: \'#%4$s\',
-				  color: \'#%5$s\'
-				},
-				tweets: {
-				  background: \'#%6$s\',
-				  color: \'#%7$s\',
-				  links: \'#%8$s\'
-				}
-			  },
-			  features: {
-				scrollbar: false,
-				loop: false,
-				live: false,
-				behavior: \'all\'
-			  }
-			}).render().setUser(\'%9$s\').start();
+			jQuery(document).ready(function(){
+				var twitterWidget = new TWTR.Widget({
+				  version: 2,
+				  type: \'profile\',
+				  rpp: %10$s,
+				  interval: %2$s,
+				  width: %2$s,
+				  height: %3$s,
+				  id: \'twitter-widget-content\',
+				  theme: {
+					shell: {
+					  background: \'#%4$s\',
+					  color: \'#%5$s\'
+					},
+					tweets: {
+					  background: \'#%6$s\',
+					  color: \'#%7$s\',
+					  links: \'#%8$s\'
+					}
+				  },
+				  features: {
+					scrollbar: false,
+					loop: false,
+					live: false,
+					behavior: \'all\',
+					avatars: %11$s
+				  }
+				});
+				
+				twitterWidget.render().setUser(\'%9$s\').start();
+			});
 			</script>',
 			$instance['interval'],
 			$instance['twitter_width'],
@@ -111,7 +121,9 @@ class CFX_Twitter_Widget extends WP_Widget {
 			$instance['tweets_bg'],
 			$instance['tweets_color'],
 			$instance['tweets_links'],
-			$twitteruser
+			$twitteruser,
+			$instance['tweets_num'],
+			$instance['show_avatar']
 		);
         
         //Widget Body Stop
@@ -140,6 +152,19 @@ class CFX_Twitter_Widget extends WP_Widget {
             <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title', 'calibrefx'); ?>:</label>
             <input type="text" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo esc_attr($instance['title']); ?>" class="widefat" />
         </p>
+		
+		<p>
+            <label for="<?php echo $this->get_field_id('tweets_num'); ?>"><?php _e('Number of Tweets', 'calibrefx'); ?>:</label>
+            <input type="text" id="<?php echo $this->get_field_id('tweets_num'); ?>" name="<?php echo $this->get_field_name('tweets_num'); ?>" value="<?php echo esc_attr($instance['tweets_num']); ?>" class="widefat" />
+        </p>
+		
+		<p>
+            <label for="<?php echo $this->get_field_id('show_avatar'); ?>"><?php _e('Show Avatar', 'calibrefx'); ?>:</label>
+			<select id="<?php echo $this->get_field_id('show_avatar'); ?>" name="<?php echo $this->get_field_name('show_avatar'); ?>">
+				<option value="true"<?php if($instance['show_avatar']=='true'){ echo ' selected="selected"'; } ?>>Yes</option>
+				<option value="false"<?php if($instance['show_avatar']=='false'){ echo ' selected="selected"'; } ?>>No</option>
+			</select>
+		</p>
 
         <p>
             <label for="<?php echo $this->get_field_id('interval'); ?>"><?php _e('Twitter Interval', 'calibrefx'); ?>:</label>
