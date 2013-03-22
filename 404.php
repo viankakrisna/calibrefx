895fe065ed8525ac5d65c2ab41c3c307ac735f7f
Hilaladdiyar Muhammad Nur <hilaladdiyar@gmail.com>
2013-03-22 17:12:43 +0700

    update 404.page & archive page

--- 404.php
+++ 404.php
@@ -34,7 +34,7 @@ function calibrefx_404() { ?>
 		<div class="entry-content">
 			<p><?php printf( __( 'The page you are looking for no longer exists. Perhaps you can return back to the site\'s <a href="%s">homepage</a> and see if you can find what you are looking for. Or, you can try finding it with the information below.', 'calibrefx' ), home_url() ); ?></p>
 
-			<div class="row">
+			<div class="<?php row_class(); ?>">
 				<div class="archive-page">
 
 					<h4><?php _e( 'Pages:', 'calibrefx' ); ?></h4>
--- page-archive.php
+++ page-archive.php
@@ -35,40 +35,40 @@ add_action('calibrefx_post_content', 'calibrefx_do_archive_content');
  */
 function calibrefx_do_archive_content() {
     ?>
+    <div class="<?php row_class(); ?>">
+        <div class="archive-page">
 
-    <div class="archive-page">
+            <h4><?php _e('Pages:', 'calibrefx'); ?></h4>
+            <ul>
+        <?php wp_list_pages('title_li='); ?>
+            </ul>
 
-        <h4><?php _e('Pages:', 'calibrefx'); ?></h4>
-        <ul>
-    <?php wp_list_pages('title_li='); ?>
-        </ul>
+            <h4><?php _e('Categories:', 'calibrefx'); ?></h4>
+            <ul>
+        <?php wp_list_categories('sort_column=name&title_li='); ?>
+            </ul>
 
-        <h4><?php _e('Categories:', 'calibrefx'); ?></h4>
-        <ul>
-    <?php wp_list_categories('sort_column=name&title_li='); ?>
-        </ul>
+        </div><!-- end .archive-page-->
 
-    </div><!-- end .archive-page-->
+        <div class="archive-page">
 
-    <div class="archive-page">
+            <h4><?php _e('Authors:', 'calibrefx'); ?></h4>
+            <ul>
+        <?php wp_list_authors('exclude_admin=0&optioncount=1'); ?>
+            </ul>
 
-        <h4><?php _e('Authors:', 'calibrefx'); ?></h4>
-        <ul>
-    <?php wp_list_authors('exclude_admin=0&optioncount=1'); ?>
-        </ul>
+            <h4><?php _e('Monthly:', 'calibrefx'); ?></h4>
+            <ul>
+        <?php wp_get_archives('type=monthly'); ?>
+            </ul>
 
-        <h4><?php _e('Monthly:', 'calibrefx'); ?></h4>
-        <ul>
-    <?php wp_get_archives('type=monthly'); ?>
-        </ul>
+            <h4><?php _e('Recent Posts:', 'calibrefx'); ?></h4>
+            <ul>
+        <?php wp_get_archives('type=postbypost&limit=100'); ?>
+            </ul>
 
-        <h4><?php _e('Recent Posts:', 'calibrefx'); ?></h4>
-        <ul>
-    <?php wp_get_archives('type=postbypost&limit=100'); ?>
-        </ul>
-
-    </div><!-- end .archive-page-->
-
+        </div><!-- end .archive-page-->
+    </div>
     <?php
 }
 
