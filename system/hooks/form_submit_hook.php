4469c6966eb8c139c610eb57bc975d7faa229e96
Hilaladdiyar Muhammad Nur <hilal@calibrefx>
2013-06-20 10:40:19 +0700

    add autoresponder submit handler

--- system/hooks/form_submit_hook.php
+++ system/hooks/form_submit_hook.php
@@ -54,10 +54,108 @@ function form_submit_handler(){
 
 				@wp_mail( $target , __('Contact Us Form Submitted on ','calibrefx').get_option('blogname'), $output_message, $headers);
 
-				wp_redirect( $redirect.'?submitted=true' ); exit;
+				wp_redirect( $redirect.'?submitted=true&type=contactform' ); exit;
 
-			break;
+				break;
+			case 'submit-autoresponder':
+				$autoresponder_type = $CFX->other_settings_m->get('autoresponder_type');
 
+				$success = false;
+
+				$name = sanitize_text_field($_POST['name']);
+				$email = sanitize_text_field($_POST['email']);
+
+				// Checking for the value of name 
+				if(empty($name)){
+					wp_redirect(site_url()."/?submitted=1&error=1&type=autoresponder");
+					exit;
+				}
+
+				// Checking for the value of email
+				if(empty($email)){
+					wp_redirect(site_url()."/?submitted=1&error=2&type=autoresponder");
+					exit;
+				}
+
+				// Checking validity for the email
+				if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+					wp_redirect(site_url()."/?submitted=1&error=3&type=autoresponder");
+					exit;
+				}
+
+				$parts = explode("@", $email);
+				$user_login = $parts[0];
+
+				if($autoresponder_type == 'mailventure'){
+					$list_id = $CFX->other_settings_m->get('autoreponder_mailventure_campaign');
+					$form_id = $CFX->other_settings_m->get('autoreponder_mailventure_form');
+
+					$ac = new MailVenture(MAILVENTURE_URL, MAILVENTURE_API_KEY); //die_dump($ac);
+					
+					// TO DO: error credentials
+					if (!$ac->credentials_test()) {
+						calibrefx_log_message('debug','Invalid credentials (URL and API Key).');
+						wp_redirect(site_url()."/?submitted=1&error=4&type=autoresponder");
+						exit;
+					}
+
+					// CHECK IF THEY EXIST FIRST.
+					$subscriber_exists = $ac->api("subscriber/view?email=$email");
+
+					if($_SERVER['REMOTE_ADDR'] != '::1'){ // if the IP is not from local IP
+						$subscriber = array(
+							"email" => $email,
+							"first_name" => $name,
+							"p[{$list_id}]" => $list_id,
+							"status[{$list_id}]" => '1', // add as "Unsubscribed",
+							"form"=> $form_id,
+							"ip4"=> $_SERVER['REMOTE_ADDR'],
+							"sdate[{$list_id}]"=> date('Y-m-d')
+						);
+					}else{ // it's form local IP
+						$subscriber = array(
+							"email" => $email,
+							"first_name" => $name,
+							"p[{$list_id}]" => $list_id,
+							"status[{$list_id}]" => '1', // add as "Unsubscribed",
+							"form"=> $form_id,
+							"ip4"=> '127.0.0.1',
+							"sdate[{$list_id}]"=> date('Y-m-d')
+						);
+					}
+
+					$subscriber_add = $ac->api("subscriber/add", $subscriber);
+
+					$subscriber_id = $subscriber_add->subscriber_id;
+					calibrefx_log_message('debug','Subscription success. Subscriber id: '.$subscriber_id);
+
+					if($subscriber_id){
+						$success = true;
+					}
+				}elseif($autoresponder_type == 'aweber'){
+					$form = stripslashes($CFX->other_settings_m->get('autoreponder_aweber'));
+
+					if(calibrefx_submit_aweber($form, $name, $email )){
+						$success = true;
+					}
+				}elseif($autoresponder_type == 'getresponse'){
+					$api_key = $CFX->other_settings_m->get('autoreponder_getresponse_api');
+					$campaign_name = $CFX->other_settings_m->get('autoreponder_getresponse_campaign');
+
+					if(calibrefx_submit_getresponse_api($name, $email, $api_key, $campaign_name)){
+						$success = true;
+					}
+				}
+
+				if($success){
+					wp_redirect(site_url()."?submitted=1&success=1&type=autoresponder");
+					exit;
+				}else{
+					wp_redirect(site_url()."?submitted=1&success=0&type=autoresponder");
+					exit;
+				}
+
+				break;
 			default : break;
 		}
 	}
