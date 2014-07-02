f37a42d0b26a808dcdf3265447d61d4a0522d58f
sean <sean@calibreworks.co.id>
2014-07-02 20:06:16 +0700

    installed nProgress

--- assets/js/calibrefx.js
+++ assets/js/calibrefx.js
@@ -1,10 +1,47 @@
 (function(){
+
+
+
 var special = jQuery.event.special, uid1 = 'D' + (+new Date()), uid2 = 'D' + (+new Date() + 1);
 special.scrollstart = {setup: function() {var timer, handler =  function(evt) {var _self = this, _args = arguments;if (timer) { clearTimeout(timer); } else { evt.type = 'scrollstart';jQuery.event.handle.apply(_self, _args);}timer = setTimeout( function(){timer = null;}, special.scrollstop.latency);};jQuery(this).bind('scroll', handler).data(uid1, handler);},teardown: function(){jQuery(this).unbind( 'scroll', jQuery(this).data(uid1) );}};
 special.scrollstop = {latency: 300,setup: function() {var timer, handler = function(evt) {var _self = this, _args = arguments;if (timer) {clearTimeout(timer);}timer = setTimeout( function(){timer = null;evt.type = 'scrollstop';jQuery.event.handle.apply(_self, _args);}, special.scrollstop.latency);};jQuery(this).bind('scroll', handler).data(uid2, handler);},teardown: function() { jQuery(this).unbind( 'scroll', jQuery(this).data(uid2) );}};
 })();
 
+	jQuery(window).load(function(){
+	   NProgress.done();
+
+	});
+
+
+	jQuery(document).ready(function() {
+	   NProgress.start();
+
+	});
+
 jQuery(document).ready(function($) {
+
+	
+
+	function imageLoaded() {
+       // function to invoke for loaded image
+       NProgress.inc();
+
+    }
+
+    $('img').each(function() {
+
+        if( this.complete ) {
+            imageLoaded.call( this );
+        } 
+
+        else {
+            $(this).one('load', imageLoaded);
+        }
+
+    });
+
+
+
 	$("#commentform, #calibrefx_contact_form").validate();
 	
 	$('#header .menu, .superfish').superfish({
@@ -67,4 +104,7 @@ jQuery(document).ready(function($) {
 
 		return false;
 	})
+
 });
+
+	
