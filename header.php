72ba6270d462929c1526991ea4ec4bd2f453f2d1
Sofyan Sitorus <sofyansitorus@gmail.com>
2015-02-22 14:00:03 +0700

    Fix viewport attribute on header for responsive layout

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
         echo '<link rel="pingback" href="' . get_bloginfo( 'pingback_url' ) . '" />' . "\n";
     }
 	if ( current_theme_supports( 'calibrefx-responsive-style' ) ){ 
-		echo '<meta name="viewport" content="width=device-width, initial-scale=1.0" />';
+		echo '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />' . "\n";
     }
 	do_action( 'calibrefx_meta' );
 	wp_head();
