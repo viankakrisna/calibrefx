1b6a03a13e868e120ba6e57c5ebcf583893e6780
Ivan Kristianto <ivan.kristianto@gmail.com>
2015-02-24 15:25:52 +0700

    adjust test case

--- tests/test_class.calibrefx.php
+++ tests/test_class.calibrefx.php
@@ -21,7 +21,7 @@ class WP_Test_Calibrefx extends WP_UnitTestCase {
 		$calibrefx = Calibrefx::get_instance();
 
 		$this->assertTrue( current_theme_supports( 'calibrefx-admin-menu' ) );
-		$this->assertTrue( current_theme_supports( 'calibrefx-custom-header' ) );
+		$this->assertTrue( current_theme_supports( 'custom-header' ) );
 		$this->assertTrue( current_theme_supports( 'calibrefx-custom-background' ) );
 		$this->assertTrue( current_theme_supports( 'calibrefx-default-styles' ) );
 		$this->assertTrue( current_theme_supports( 'calibrefx-inpost-layouts' ) );
