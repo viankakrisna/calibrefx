cf95c2bf5a6991a1e87a343ad0e6cc3277598536
Sofyan Sitorus <sofyansitorus@gmail.com>
2015-04-22 13:33:10 +0700

    Update class.calibrefx-admin.php
    Bug Fix: Notice: Undefined index: calibrefx-settings in \wp-content\themes\calibrefx\system\class.calibrefx-admin.php on line 127

--- system/class.calibrefx-admin.php
+++ system/class.calibrefx-admin.php
@@ -1,227 +1,237 @@
-<?php 
+<?php
 /**
  * Calibrefx Admin Class
  *
  * Abstract class to build Admin page
- * 
+ *
  */
 abstract class Calibrefx_Admin {
 
-    /**
-     * Hold pagehook value when menu is registered
-     *
-     * @var string
-     */
-    public $pagehook;
+	/**
+	 * Hold pagehook value when menu is registered
+	 *
+	 * @var string
+	 */
+	public $pagehook;
 
-    /**
-     * ID of the admin menu and settings page.
-     *
-     * @var string
-     */
-    public $page_id;
+	/**
+	 * ID of the admin menu and settings page.
+	 *
+	 * @var string
+	 */
+	public $page_id;
 
-    /**
-     * Name of the settings field in the options table.
-     *
-     * @var string
-     */
-    public $settings_field;
+	/**
+	 * Name of the settings field in the options table.
+	 *
+	 * @var string
+	 */
+	public $settings_field;
 
-    /**
-     * Hold default settings of the settings option
-     *
-     * @var string
-     */
-    public $default_settings;
-   
-    /**
-     * Remove Post Form
-     *
-     * @var object
-     */
-    public $_form_method = 'post';
+	/**
+	 * Hold default settings of the settings option
+	 *
+	 * @var string
+	 */
+	public $default_settings;
+
+	/**
+	 * Remove Post Form
+	 *
+	 * @var object
+	 */
+	public $_form_method = 'post';
 
 
-    public $_submit_url = 'options.php';
+	public $_submit_url = 'options.php';
 
-    /**
-     * Constructor
-     */
-    function __construct( $settings_field = 'calibrefx-settings' ) {
-        $this->settings_field = $settings_field;
-    }
+	/**
+	 * Constructor
+	 */
+	function __construct( $settings_field = 'calibrefx-settings' ) {
+		$this->settings_field = $settings_field;
+	}
 
-    /**
-     * Initialize our admin area
-     * 
-     */
-    public function initialize() {
-        global $calibrefx;
+	/**
+	 * Initialize our admin area
+	 *
+	 */
+	public function initialize() {
+		global $calibrefx;
 
-        add_action( 'calibrefx_hidden_fields', array( $this,'hidden_fields' ) );
-        add_action( 'admin_init', array( $this, 'register_settings' ), 5 );
-        add_action( 'admin_init', array( $this, 'settings_init' ), 20 );
-        add_filter( 'admin_body_class', 'calibrefx_admin_body_class', 20, 1 );
-        
-        add_action( 'admin_notices', array( $this, 'notices' ) );
-        
-        //We add the security layer here
-        $this->security_filters();
+		add_action( 'calibrefx_hidden_fields', array( $this, 'hidden_fields' ) );
+		add_action( 'admin_init', array( $this, 'register_settings' ), 5 );
+		add_action( 'admin_init', array( $this, 'settings_init' ), 20 );
+		add_filter( 'admin_body_class', 'calibrefx_admin_body_class', 20, 1 );
 
-        if ( $_SERVER['REQUEST_METHOD'] !== 'POST' ) return;
+		add_action( 'admin_notices', array( $this, 'notices' ) );
 
-        do_action( 'calibrefx_before_save_core' );
+		//We add the security layer here
+		$this->security_filters();
 
-        /** Add a sanitizer/validator */
-        add_filter( 'pre_update_option_' . $this->settings_field, array( &$this, 'save' ), 10, 2 );
+		if ( ! isset( $_SERVER['REQUEST_METHOD'] ) OR 'POST' !== esc_attr( $_SERVER['REQUEST_METHOD'] ) ) {
+			return;
+		}
 
-        do_action('calibrefx_after_save_core');
-    }
+		do_action( 'calibrefx_before_save_core' );
 
-    /**
-     * Register Our Security Filters
-     *
-     * $return void
-     */
-    protected function security_filters() {}
-    
-    /**
-     * Register our meta boxes
-     *
-     * $return null
-     */
-    abstract public function meta_boxes();
+		/** Add a sanitizer/validator */
+		add_filter( 'pre_update_option_' . $this->settings_field, array( &$this, 'save' ), 10, 2 );
 
-    /**
-     * Register our meta sections
-     *
-     * $return null
-     */
-    abstract public function meta_sections();
+		do_action( 'calibrefx_after_save_core' );
+	}
 
-    public function hidden_fields() {
-        
-    }
+	/**
+	 * Register Our Security Filters
+	 *
+	 * $return void
+	 */
+	protected function security_filters() {}
 
-    /**
-     * Save our settings option
-     *
-     * $return array
-     */
-    public function save( $_newvalue, $_oldvalue ) { 
-        //We merge newvalue and oldvalue
-        if ( isset( $_newvalue['reset'] ) ) {
-            return $_newvalue;
-        }
+	/**
+	 * Register our meta boxes
+	 *
+	 * $return null
+	 */
+	abstract public function meta_boxes();
 
-        if( !empty( $_POST['calibrefx_do_import'] ) ) {
-            return $_newvalue;
-        }
-        
-        //Get the value from post settings
-        $_newvalue = $_POST[ $this->settings_field ]; 
-        
-        //merge value from old settings
-        if( !is_array( $_oldvalue ) ) $_oldvalue = array();
-        if( !is_array( $_newvalue ) ) $_newvalue = array();
-        
-        $_newvalue = array_merge( $_oldvalue, $_newvalue );
-        
-        //We merge with default value too
-        $_newvalue = array_merge( (array) $this->default_settings, $_newvalue );
+	/**
+	 * Register our meta sections
+	 *
+	 * $return null
+	 */
+	abstract public function meta_sections();
 
-        if(!empty( $_newvalue ) ) {
-            //We sanitizing
-            global $calibrefx;
-            $_newvalue = $calibrefx->security->sanitize_input( $this->settings_field, $_newvalue );
-        }
-        
-        //flush wp_cache
-        wp_cache_flush();
+	public function hidden_fields() {
 
-        return $_newvalue;
-    }
+	}
 
-    /**
-     * Register the settings option in wp_options.
-     *
-     * @return null Returns early if not on the correct admin page.
-     */
-    public function register_settings() {
+	/**
+	 * Save our settings option
+	 *
+	 * $return array
+	 */
+	public function save( $_newvalue, $_oldvalue ) {
+		
+		//We merge newvalue and oldvalue
+		if ( isset( $_newvalue['reset'] ) ) {
+			return $_newvalue;
+		}
 
-        /** If this page doesn't store settings, no need to register them */
-        if ( !$this->settings_field ){
-            return;
-        }
+		if ( ! empty( $_POST['calibrefx_do_import'] ) ) {
+			return $_newvalue;
+		}
 
-        register_setting( $this->settings_field, $this->settings_field );
-        add_option( $this->settings_field, $this->default_settings );
+		//Get the value from post settings
 
-        if ( !isset( $_REQUEST['page'] ) || $_REQUEST['page'] != $this->page_id ){
-            return;
-        }
+		$_newvalue = isset($_POST[ $this->settings_field ]) ? $_POST[ $this->settings_field ] : array();
 
-        if ( calibrefx_get_option( 'reset', $this->settings_field ) ) {
-            if ( update_option( $this->settings_field, $this->default_settings ) ){
-                calibrefx_admin_redirect( $this->page_id, array( 'reset' => 'true' ) );
-            } else {
-                calibrefx_admin_redirect( $this->page_id, array( 'error' => 'true' ) );
-            }
-            exit;
-        }
-    }
+		if($_newvalue){
+			$_newvalue = (is_array($_newvalue)) ? array_map( 'esc_attr', $_POST[ $this->settings_field ] ) : esc_attr( $_POST[ $this->settings_field ] );
+		}
 
-    /**
-     * Display notices on the save or reset of settings.
-     *
-     * @return null Returns early if not on the correct admin page.
-     */
-    public function notices() {
-        if ( !isset( $_REQUEST['page'] ) || $_REQUEST['page'] != $this->page_id ){
-            return;
-        }
-        
-        if ( isset( $_REQUEST['settings-updated']) && $_REQUEST['settings-updated'] == 'true' ){
-            echo '<div id="message" class="updated"><p><strong>' . __( 'Settings saved.', 'calibrefx' ) . '</strong></p></div>';
-        } elseif ( isset( $_REQUEST['reset'] ) && 'true' == $_REQUEST['reset'] ) {
-            echo '<div id="message" class="updated"><p><strong>' . __( 'Settings reset.', 'calibrefx' ) . '</strong></p></div>';
-        } elseif ( isset( $_REQUEST['error'] ) && $_REQUEST['error'] == 'true' ){
-            echo '<div id="message" class="updated"><p><strong>' . __( 'Settings not saved. Error occured.', 'calibrefx' ) . '</strong></p></div>';
-        } elseif ( isset( $_REQUEST['import'] ) && $_REQUEST['import'] == 'true' ){
-            echo '<div id="message" class="updated"><p><strong>' . __( 'Import Settings Success.', 'calibrefx' ) . '</strong></p></div>';
-        }
-    }
 
-    public function settings_init() {
-        add_action( 'load-' . $this->pagehook, array( $this, 'scripts' ) );
-        add_action( 'load-' . $this->pagehook, array( $this, 'styles' ) );
-        add_action( 'load-' . $this->pagehook, array( $this, 'meta_sections' ) );
-        add_action( 'load-' . $this->pagehook, array( $this, 'meta_boxes' ) );
-    }
+		//merge value from old settings
+		if ( ! is_array( $_oldvalue ) ) { $_oldvalue = array(); }
+		if ( ! is_array( $_newvalue ) ) { $_newvalue = array(); }
 
-    public function scripts() {
-        wp_enqueue_script( 'common' );
-        wp_enqueue_script( 'wp-lists' );
-        wp_enqueue_script( 'postbox' );
-    }
+		$_newvalue = array_merge( $_oldvalue, $_newvalue );
 
-    public function styles() {
-        wp_enqueue_style( 'calibrefx_admin_css' );
-    }
+		//We merge with default value too
+		$_newvalue = array_merge( (array) $this->default_settings, $_newvalue );
 
-    public function dashboard() {
-        global $calibrefx_sections, 
-               $calibrefx_current_section;
 
-        $this->_submit_url = apply_filters( 'calibrefx_'.$calibrefx_current_section.'_form_url', 'options.php' );
+		if ( ! empty( $_newvalue ) ) {
+			//We sanitizing
+			global $calibrefx;
+			$_newvalue = $calibrefx->security->sanitize_input( $this->settings_field, $_newvalue );
+		}
 
-        ?>
+		//flush wp_cache
+		wp_cache_flush();
+
+		return $_newvalue;
+	}
+
+	/**
+	 * Register the settings option in wp_options.
+	 *
+	 * @return null Returns early if not on the correct admin page.
+	 */
+	public function register_settings() {
+
+		/** If this page doesn't store settings, no need to register them */
+		if ( ! $this->settings_field ){
+			return;
+		}
+
+		register_setting( $this->settings_field, $this->settings_field );
+		add_option( $this->settings_field, $this->default_settings );
+
+		if ( ! isset( $_REQUEST['page'] ) OR $this->page_id != absint( $_REQUEST['page'] ) ){
+			return;
+		}
+
+		if ( calibrefx_get_option( 'reset', $this->settings_field ) ) {
+			if ( update_option( $this->settings_field, $this->default_settings ) ){
+				calibrefx_admin_redirect( $this->page_id, array( 'reset' => 'true' ) );
+			} else {
+				calibrefx_admin_redirect( $this->page_id, array( 'error' => 'true' ) );
+			}
+			exit;
+		}
+	}
+
+	/**
+	 * Display notices on the save or reset of settings.
+	 *
+	 * @return null Returns early if not on the correct admin page.
+	 */
+	public function notices() {
+		if ( ! isset( $_REQUEST['page'] ) OR $this->page_id != $_REQUEST['page'] ){
+			return;
+		}
+
+		if ( isset( $_REQUEST['settings-updated'] ) AND 'true' == $_REQUEST['settings-updated'] ){
+			echo '<div id="message" class="updated"><p><strong>' . __( 'Settings saved.', 'calibrefx' ) . '</strong></p></div>';
+		} elseif ( isset( $_REQUEST['reset'] ) AND 'true' == $_REQUEST['reset'] ) {
+			echo '<div id="message" class="updated"><p><strong>' . __( 'Settings reset.', 'calibrefx' ) . '</strong></p></div>';
+		} elseif ( isset( $_REQUEST['error'] ) AND 'true' == $_REQUEST['error'] ){
+			echo '<div id="message" class="updated"><p><strong>' . __( 'Settings not saved. Error occured.', 'calibrefx' ) . '</strong></p></div>';
+		} elseif ( isset( $_REQUEST['import'] ) AND 'true' == $_REQUEST['import'] ){
+			echo '<div id="message" class="updated"><p><strong>' . __( 'Import Settings Success.', 'calibrefx' ) . '</strong></p></div>';
+		}
+	}
+
+	public function settings_init() {
+		add_action( 'load-' . $this->pagehook, array( $this, 'scripts' ) );
+		add_action( 'load-' . $this->pagehook, array( $this, 'styles' ) );
+		add_action( 'load-' . $this->pagehook, array( $this, 'meta_sections' ) );
+		add_action( 'load-' . $this->pagehook, array( $this, 'meta_boxes' ) );
+	}
+
+	public function scripts() {
+		wp_enqueue_script( 'common' );
+		wp_enqueue_script( 'wp-lists' );
+		wp_enqueue_script( 'postbox' );
+	}
+
+	public function styles() {
+		wp_enqueue_style( 'calibrefx_admin_css' );
+	}
+
+	public function dashboard() {
+		global $calibrefx_sections,
+			   $calibrefx_current_section;
+
+		$this->_submit_url = apply_filters( 'calibrefx_'.$calibrefx_current_section.'_form_url', 'options.php' );
+
+		?>
         <div id="<?php echo $this->settings_field;?>-page" class="wrap calibrefx-metaboxes <?php echo $calibrefx_current_section; ?>">
             <form method="<?php echo $this->_form_method; ?>" action="<?php echo $this->_submit_url; ?>" enctype="multipart/form-data" id="calibrefx-form">
-                <?php if( $this->_form_method == 'post' ) : ?>
-                <?php wp_nonce_field( 'closedpostboxes', 'closedpostboxesnonce', false); ?>
-                <?php wp_nonce_field( 'meta-box-order', 'meta-box-order-nonce', false); ?>
+                <?php if ( 'post' == $this->_form_method ) : ?>
+                <?php wp_nonce_field( 'closedpostboxes', 'closedpostboxesnonce', false ); ?>
+                <?php wp_nonce_field( 'meta-box-order', 'meta-box-order-nonce', false ); ?>
                 <?php settings_fields( $this->settings_field ); // important! ?>
                 <?php do_action( 'calibrefx_hidden_fields' ); ?>
                 <?php endif; ?>
@@ -230,11 +240,11 @@ abstract class Calibrefx_Admin {
                     <div class="calibrefx-option-logo">
                         <a target="_blank" href="http://www.calibrefx.com" title="CalibreFx">&nbsp;</a>
                     </div>
-                    <?php if( $this->_form_method == 'post' ) : ?>
+                    <?php if ( 'post' == $this->_form_method ) : ?>
                     <div class="calibrefx-submit-button">
                         <div class="clear"></div>
                         <button type="submit" class="calibrefx-h2-button calibrefx-settings-submit-button"><i class="fa fa-save fa-2"></i> <?php _e( 'Save Settings', 'calibrefx' ) ?></button>
-                        <button type="submit" class="calibrefx-h2-button calibrefx-settings-reset-button" name="<?php echo $this->settings_field; ?>[reset]" value="1" onclick="return calibrefx_confirm( '<?php echo esc_js(__( 'Are you sure you want to reset?', 'calibrefx' ) ); ?>' );"><i class="fa fa-refresh fa-2"></i><?php _e( 'Reset Settings', 'calibrefx' ); ?></button>
+                        <button type="submit" class="calibrefx-h2-button calibrefx-settings-reset-button" name="<?php echo $this->settings_field; ?>[reset]" value="1" onclick="return calibrefx_confirm( '<?php echo esc_js( __( 'Are you sure you want to reset?', 'calibrefx' ) ); ?>' );"><i class="fa fa-refresh fa-2"></i><?php _e( 'Reset Settings', 'calibrefx' ); ?></button>
                     </div>
                     <?php endif; ?>
                 </div>
@@ -246,27 +256,27 @@ abstract class Calibrefx_Admin {
                         <div class="calibrefx-tab">
                             <ul class="calibrefx-tab-option">
                                 <?php
-                                foreach ( $calibrefx_sections as $section ) {
-                                    $current_class = ( $calibrefx_current_section === $section['slug'] ) ? ' class="current"' : '';
-                                    $section_link = admin_url( 'admin.php?page=' . $this->page_id . '&section=' . $section['slug'] );
-                                    
-                                    $icon = $section['icon'];
+								foreach ( $calibrefx_sections as $section ) {
+									$current_class = ( $calibrefx_current_section === $section['slug'] ) ? ' class="current"' : '';
+									$section_link = admin_url( 'admin.php?page=' . $this->page_id . '&section=' . $section['slug'] );
 
-                                    echo "<li$current_class>
+									$icon = $section['icon'];
+
+									echo "<li$current_class>
                                             <a href='$section_link'>
-                                                <span class='calibrefx-section-link'><i class='$icon'></i>" . $section['title'] . "</span>
-                                                <!--span class='calibrefx-section-link-additional'></span-->
-                                            </a>
-                                        </li>";
-                                }
-                                ?>
+                                                <span class='calibrefx-section-link'><i class='$icon'></i>" . esc_attr( $section['title'] ) . "</span>
+												<!--span class='calibrefx-section-link-additional'></span-->
+											</a>
+										</li>";
+								}
+								?>
                             </ul>
                             <div class="calibrefx-option">
-                                <h2><?php echo $calibrefx_sections[$calibrefx_current_section]['title']; ?></h2>
+                                <h2><?php echo esc_attr( $calibrefx_sections[ $calibrefx_current_section ]['title'] ); ?></h2>
                                 <div class="postbox-container main-postbox">
                                     <?php
-                                    calibrefx_do_meta_sections( $calibrefx_current_section, $this->pagehook, 'main', null );
-                                    ?>
+									calibrefx_do_meta_sections( $calibrefx_current_section, $this->pagehook, 'main', null );
+									?>
                                 </div>
 
                                 <div class="clear"></div>
@@ -289,12 +299,12 @@ abstract class Calibrefx_Admin {
                 // close postboxes that should be closed
                 $( '.if-js-closed' ).removeClass( 'if-js-closed' ).addClass( 'closed' );
                 // postboxes setup
-                postboxes.add_postbox_toggles( '<?php echo $this->pagehook; ?>' );
+                postboxes.add_postbox_toggles( '<?php echo esc_attr( $this->pagehook ); ?>' );
                             
                 postboxes._mark_area = function() {
                     var visible = $( 'div.postbox:visible' ).length, side = $( '#post-body #side-sortables' );
 
-                    $( '#<?php echo $this->pagehook; ?> .meta-box-sortables:visible' ).each(function(n, el) {
+                    $( '#<?php echo esc_attr( $this->pagehook ); ?> .meta-box-sortables:visible' ).each(function(n, el) {
                         var t = $( this );
 
                         if ( visible == 1 || t.children( '.postbox:visible' ).length )
@@ -337,6 +347,6 @@ abstract class Calibrefx_Admin {
             //]]>
         </script>
         <?php
-    }
+	}
 
 }
